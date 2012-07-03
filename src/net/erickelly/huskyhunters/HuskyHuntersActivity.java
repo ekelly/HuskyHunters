c70d9edb17176ef91e488b285ef8a06f4ceee0e2
Eric Kelly <me@useric.com>
2012-07-03 11:56:09 -0400

    first commit

---
+++ src/net/erickelly/huskyhunters/HuskyHuntersActivity.java
@@ -0,0 +1,12 @@
+package net.erickelly.huskyhunters;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HuskyHuntersActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
