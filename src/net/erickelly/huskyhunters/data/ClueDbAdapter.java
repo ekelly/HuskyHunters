6cdcd37039766e918289381a2ea5651396833530
fnimick <fnimick@gmail.com>
2012-07-05 17:01:06 -0400

    Started database adapter design

---
+++ src/net/erickelly/huskyhunters/data/ClueDbAdapter.java
@@ -0,0 +1,29 @@
+package net.erickelly.huskyhunters.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ClueDbAdapter extends SQLiteOpenHelper {
+	public static final String TAG = "ClueDbAdapter";
+	
+	public ClueDbAdapter(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/net/erickelly/huskyhunters/data/DbConstants.java
+++ src/net/erickelly/huskyhunters/data/DbConstants.java
@@ -1,9 +1,18 @@
 package net.erickelly.huskyhunters.data;
 
+import android.provider.BaseColumns;
+
+/**
+ * 
+ * @author francis
+ *
+ */
 public final class DbConstants {
 	public static final String DB_NAME = "cluedata";
+	public static final int DB_VERSION = 1;
 	
 	public static final String ROWID = "_id";
+	public static final String TIME = "time";
 	
 	// server data table
 	public static final String SERVER_TABLE = "serverTable";
@@ -14,7 +23,7 @@ public final class DbConstants {
 	public static final String SERVER_POINTS = "points";
 	public static final String SERVER_PICS_REQD = "picsreqd";
 	public static final String SERVER_PICS_ON_SERVER = "picsonserver";
-	public static final String 
+	
 	
 	// device data table
 	public static final String DEVICE_TABLE = "deviceTable";
@@ -30,14 +39,44 @@ public final class DbConstants {
 	
 	
 	
+	// table creation strings
+	public static final String SQL_DEVICE_TABLE_CREATION =
+			"CREATE TABLE " + DEVICE_TABLE + " (" + ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ DEVICE_CLUEID + " TEXT NOT NULL UNIQUE, " + DEVICE_PICS_ON_DEVICE + " INTEGER NOT NULL);";
+	public static final String SQL_PHOTO_TABLE_CREATION = 
+			"CREATE TABLE " + PHOTO_TABLE + " (" + ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ PHOTO_PICURI + " TEXT NOT NULL, " + PHOTO_CRTIME + " INTEGER NOT NULL);";
 	
-	
-	
-	
-	public static final String KEY_PHOTO_PATH = "photo_path";
-	public static final String KEY_LOCATION = "location";
-	public static final String KEY_UPLOADED = "uploaded"; //boolean
-	public static final String KEY_ROWID = "_id";
+	// table creation string for server data - is method that takes name of table
+	// to allow for temporary table
+	/**
+	 * Table creation string for server data.  Takes name of table to provide string
+	 * for creation of temporary table for storing downloaded data.
+	 * @param tableName
+	 * @return a SQL command to create a table of the given name.
+	 */
+	public static final String SQL_SERVER_TABLE_CREATION(String tableName)
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("CREATE TABLE ");
+		sb.append(tableName);
+		sb.append(" (");
+		sb.append(BaseColumns._ID);
+		sb.append(" integer primary key autoincrement, ");
+		sb.append(SERVER_CLUEID);
+		sb.append(" text not null, ");
+		sb.append(SERVER_TEXT);
+		sb.append(" text not null, ");
+		sb.append(SERVER_ANS);
+		sb.append(" text not null, ");
+		sb.append(SERVER_POINTS);
+		sb.append(" integer not null, ");
+		sb.append(SERVER_PICS_REQD);
+		sb.append(" integer not null, ");
+		sb.append(SERVER_PICS_ON_SERVER);
+		sb.append(" integer not null);");
+		return sb.toString();
+	}
 	
 	
 }
