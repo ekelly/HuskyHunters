ace30efb8bff57bd0023aac231bf11922276e6b7
fnimick <fnimick@gmail.com>
2012-07-05 12:31:33 -0400

    fixed classpath for real this time

---
+++ src/net/erickelly/huskyhunters/data/CluesProvider.java
@@ -0,0 +1,7 @@
+package net.erickelly.huskyhunters.data;
+
+import ContentProvider;
+
+public class CluesProvider extends ContentProvider {
+
+}
---
+++ src/net/erickelly/huskyhunters/data/DbConstants.java
@@ -0,0 +1,43 @@
+package net.erickelly.huskyhunters.data;
+
+public final class DbConstants {
+	public static final String DB_NAME = "cluedata";
+	
+	public static final String ROWID = "_id";
+	
+	// server data table
+	public static final String SERVER_TABLE = "serverTable";
+	//public static final String SERVER_ROWID = "_id";
+	public static final String SERVER_CLUEID = "clueid";
+	public static final String SERVER_TEXT = "cluetext";
+	public static final String SERVER_ANS = "clueanswer";
+	public static final String SERVER_POINTS = "points";
+	public static final String SERVER_PICS_REQD = "picsreqd";
+	public static final String SERVER_PICS_ON_SERVER = "picsonserver";
+	public static final String 
+	
+	// device data table
+	public static final String DEVICE_TABLE = "deviceTable";
+	//public static final String Device_ROWID = "_id";
+	public static final String DEVICE_CLUEID = "clueid";
+	public static final String DEVICE_PICS_ON_DEVICE = "picsondevice";
+	
+	// photo table
+	public static final String PHOTO_TABLE = "photoTable";
+	public static final String PHOTO_PICURI = "picUri";
+	public static final String PHOTO_CRTIME = "crtime";
+	
+	
+	
+	
+	
+	
+	
+	
+	public static final String KEY_PHOTO_PATH = "photo_path";
+	public static final String KEY_LOCATION = "location";
+	public static final String KEY_UPLOADED = "uploaded"; //boolean
+	public static final String KEY_ROWID = "_id";
+	
+	
+}
